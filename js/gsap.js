fd43a6dd5ccdeb6935260f67d6f39bc7e5fc8ffd
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-09-19 22:49:22 +0900

    gsap 적용

---
+++ js/gsap.js
@@ -0,0 +1,65 @@
+const newsTitle = document.querySelector('#news h1');
+const newsWrap = document.querySelector('#news .inner .wrap');
+const commuTitle = document.querySelector('#mainCommunity h1');
+const commuWrap = document.querySelector('#mainCommunity .inner .wrap');
+const bannerTxt = document.querySelector('#banner .inner .txtBN');
+const scienceTitle = document.querySelector('#science h1');
+const scienceWrap = document.querySelector('#science .inner .wrap');
+const goodsTitle = document.querySelector('#goods .inner h1');
+const goodsContent = document.querySelectorAll('#goods .inner ul li');
+
+const newsTl = gsap.timeline({
+	scrollTrigger: {
+		trigger: newsTitle,
+		start: 'top 50%',
+		end: 'top 0%',
+	},
+});
+
+newsTl.from(newsTitle, { x: 500, scale: 2 });
+newsTl.from(newsWrap, { x: 500, opacity: 0 });
+
+const commuTl = gsap.timeline({
+	scrollTrigger: {
+		trigger: commuTitle,
+		start: 'top 50%',
+		end: 'top 0%',
+	},
+});
+
+commuTl.from(commuTitle, { x: 500, scale: 2 });
+commuTl.from(commuWrap, { x: -500, opacity: 0 });
+
+gsap.from(bannerTxt, {
+	scale: 1.5,
+	duration: 1,
+	scrollTrigger: {
+		trigger: bannerTxt,
+		start: 'top 60%',
+		end: 'bottom 20%',
+		scrub: true,
+		markers: false,
+	},
+});
+
+const scienceTl = gsap.timeline({
+	scrollTrigger: {
+		trigger: scienceTitle,
+		start: 'top 50%',
+		end: 'top 0%',
+	},
+});
+
+scienceTl.from(scienceTitle, { x: 500, scale: 2 });
+scienceTl.from(scienceWrap, { x: -500, opacity: 0 });
+
+const goodsTl = gsap.timeline({
+	scrollTrigger: {
+		trigger: goodsTitle,
+		start: 'top 50%',
+		end: 'top 0%',
+	},
+});
+
+goodsTl.from(goodsTitle, { x: 500, scale: 2 });
+goodsTl.from(goodsContent, { x: -500, opacity: 0 });
